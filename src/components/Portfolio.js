f1a7134b69d13ae7bcf1a8fc1a5bc131010520c3
isaiahL88 <isaiah.ib@outlook.com>
2023-11-14 00:10:32 -0500

    switched to new slideshow framework

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react'
-import { Slide } from 'react-slideshow-image';
-
-import "../css/Portfolio.css"
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 import classicChair from "../assets/classic_chairs.jpg"
 import chairBright from "../assets/nice_fabric_chairs.jpg"
+import chair1 from '../assets/Chair.jpg'
+import chair2 from '../assets/chair2.jpg'
+import chair3 from '../assets/chair3.jpg'
 
 
-
-const properties = {
-    prevArrow: <button className='slideButtons'><svg xmlns="http://www.w3.org/2000/svg" height="100" viewBox="0 -960 960 960" width="100"><path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" /></svg></button>,
-    nextArrow: <button className='slideButtons'><svg xmlns="http://www.w3.org/2000/svg" height="100" viewBox="0 -960 960 960" width="100"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" /></svg></button>
-}
 
 export default class Portfolio extends Component {
 
     render() {
         return (
             <div>
-                <div id='section1'>
-                    <Slide {...properties} id="slideshow" >
-                        <div class="slide-container" key='1'>
-                            <img className="slide-img" src={classicChair} />
-                        </div>
-                        <div class="slide-container" key='2'>
-                            <img className="slide-img" src={chairBright} />
-                        </div>
+                <Splide
+                    options={{
+                        rewind: true,
+                        width: 550,
+                        height: 600,
+                        gap: '1rem'
+                    }}
+                    aria-label="chairs">
+                    <SplideSlide>
+                        <img src={classicChair} />
+                    </SplideSlide>
+                    <SplideSlide>
+                        <img src={chairBright} />
+                    </SplideSlide>
+                </Splide>
+                {/* <div id='section1'>
+                    <Slide id="slideshow" >
+                        <img className="slide-img" src={classicChair} />
+                        <img className="slide-img" src={chairBright} />
+                        <img className="slide-img" src={chair3} />
+                        <img className="slide-img" src={chair2} />
+                        <img className="slide-img" src={chair1} />
+
                     </Slide >
-                </div>
+                </div> */}
                 <div id='section2'>
                 </div>
                 <div id='section3'>
 
                 </div>
-            </div>
+            </div >
         )
     }
 }
