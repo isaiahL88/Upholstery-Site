489d065c293edce9a16bdd7ca1f1e709047eec29
isaiahL88 <isaiah.ib@outlook.com>
2023-11-16 13:40:23 -0500

    Finished up adding more content to home page, fixing captions on main slider, and also finished the mobile view for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,8 @@ import sofa3 from '../assets/sofa3.jpg'
 import sofa4 from '../assets/sofa4.jpg'
 import sofa_home from '../assets/sofa_home_page.jpg'
 import sofaModern from '../assets/modern_sofa.jpg'
+import fabric from '../assets/fabric.jpeg'
+import sowing from '../assets/sowing.jpeg'
 
 import 'react-slideshow-image/dist/styles.css'
 
@@ -20,34 +22,54 @@ const options = {
     gap: '1rem',
     interval: 4000,
     speed: 30,
-    autoplay: "play"
+    autoplay: "pause"
 };
 
 const optionsMobile = {
     type: 'loop',
-    width: '80vw',
-    height: '60vh',
+    width: '100vw',
+    height: '13em',
     gap: '1rem'
 };
+
+
+const mql = window.matchMedia('(max-width: 1024px)');
+let mobileView = mql.matches;
 const Home = () => {
 
     return (
         <div id="home-page">
             <div id="section-1">
-                <Splide id='splide' options={options}>
-                    <SplideSlide>
-                        {/* <p id="intro">30 years of specialization in restoration and commercial re-upholstery</p> */}
-                        <img id="section-1-img" src={sofa_home} />
+                <p id="intro">30 years of specialization in restoration and commercial re-upholstery</p>
+                {/* <h1 id="section-2-header">Make a worthwhile investment restoring and preserving your sentimental pieces of furniture</h1>
+                <h1 id="section-3-header">Refresh your restaurant or lounge banquets with a new high quality fabric!</h1> */}
+                <Splide id='splide' options={mobileView ? optionsMobile : options}>
+                    <SplideSlide className='splide_slide'>
+                        <img id="section-1-img" src={sofa_home} alt='30 years of specialization in restoration and commercial re-upholstery' />
                     </SplideSlide>
                     <SplideSlide>
-                        {/* <h1 id="section-2-header">Make a worthwhile investment restoring and preserving your sentimental pieces of furniture</h1> */}
                         <img id="section-2-img" src={sofaModern} />
                     </SplideSlide>
                     <SplideSlide>
-                        {/* <h1 id="section-3-header">Refresh your restaurant or lounge banquets with a new high quality fabric!</h1> */}
                         <img id='section-3-img' src={sofa2} />
                     </SplideSlide>
                 </Splide>
+            </div>
+            <div className='space'></div>
+            <div id='section-2'>
+                <section id='descr-2'>
+                    <h1 className='section-header'>Fabrics</h1>
+                    <p className='section-p'>Make a worthwhile investment restoring your sentimental pieces with fabrics from a wide selection</p>
+                </section>
+                <img id='img-2' src={fabric} />
+            </div>
+            <div className='space'></div>
+            <div id='section-3'>
+                <img id='img-3' src={sowing} />
+                <section id='descr-3'>
+                    <h1 className='section-header'>Sowing</h1>
+                    <p className='section-p'>Custom sowing allows us to adjust cushion, backing, and fabric lining to perfelctly fit your needs</p>
+                </section>
             </div>
         </div >
     )
