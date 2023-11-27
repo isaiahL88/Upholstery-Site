d7d0c1c95e457a477db5c52044e599ade9192084
isaiahL88 <isaiah.ib@outlook.com>
2023-11-27 15:50:22 -0500

    analytics fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./components/Footer"
 import { Route, Link, Routes } from "react-router-dom"
 import { Scrollbar } from 'smooth-scrollbar-react';
 import { Analytics } from '@vercel/analytics/react';
-// testing
+// testingff
 
 function App() {
   return (
